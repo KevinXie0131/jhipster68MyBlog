5d551b3248ddf5ed9518510b1a9c7f59076e0109
Kevin Xie <KevinX@gs1ca.org>
2020-04-13 19:20:17 -0400

    third commit

--- src/main/java/com/jhipster68myblog/com/config/CacheConfiguration.java
+++ src/main/java/com/jhipster68myblog/com/config/CacheConfiguration.java
@@ -42,6 +42,11 @@ public class CacheConfiguration {
             createCache(cm, com.jhipster68myblog.com.domain.User.class.getName());
             createCache(cm, com.jhipster68myblog.com.domain.Authority.class.getName());
             createCache(cm, com.jhipster68myblog.com.domain.User.class.getName() + ".authorities");
+            createCache(cm, com.jhipster68myblog.com.domain.Blog.class.getName());
+            createCache(cm, com.jhipster68myblog.com.domain.Entry.class.getName());
+            createCache(cm, com.jhipster68myblog.com.domain.Entry.class.getName() + ".tags");
+            createCache(cm, com.jhipster68myblog.com.domain.Tag.class.getName());
+            createCache(cm, com.jhipster68myblog.com.domain.Tag.class.getName() + ".entries");
             // jhipster-needle-ehcache-add-entry
         };
     }
